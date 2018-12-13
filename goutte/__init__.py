407e8a76b616b4679c4e80defaa95b0e4f4d7d66
Etienne Napoleone <etienne@tomochain.com>
2018-12-13 22:54:59 +0700

    refactor(logging): update logger format

--- goutte/__init__.py
+++ goutte/__init__.py
@@ -4,7 +4,10 @@ __version__ = '0.1.0'
 
 handler = colorlog.StreamHandler()
 handler.setFormatter(colorlog.ColoredFormatter(
-    '%(log_color)s%(asctime)s %(levelname)s -- %(message)s'))
+    '%(blue)s%(asctime)s%(reset)s - %(log_color)s%(levelname)s%(reset)s'
+    ' - %(yellow)s%(name)s%(reset)s - %(message)s',
+    '%H:%M:%S'
+))
 logger = colorlog.getLogger(__name__)
 logger.setLevel('INFO')
 logger.addHandler(handler)
