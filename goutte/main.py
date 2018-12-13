8ce52e187ed80b430932e4ff6b221f4b14fbc01e
etienne-napoleone <etienne@tomochain.com>
2018-12-13 19:25:12 +0700

    feat(main): remove scheduling Switching to singleshot only

--- goutte/main.py
+++ goutte/main.py
@@ -14,15 +14,13 @@ log = colorlog.getLogger(__name__)
 @click.command(help='DigitalOcean snapshot automation service')
 @click.argument('config', envvar='GOUTTE_CONFIG', type=click.File('r'))
 @click.argument('do_key', envvar='GOUTTE_DO_KEY')
-@click.option('--oneshot', is_flag=True, help='Run all tasks and then stops')
 @click.version_option(version=__version__)
-def entrypoint(config: click.File, do_key: str, oneshot: bool) -> None:
+def entrypoint(config: click.File, do_key: str) -> None:
     """Command line interface entrypoint"""
     log.info('Starting goutte v{}.'.format(__version__))
-    assert load_config(config)
 
 
-def load_config(config: click.File) -> Dict[str, Dict]:
+def _load_config(config: click.File) -> Dict[str, Dict]:
     """Return a config dict from a toml config file"""
     try:
         log.info('Loading config from {}.'.format(config.name))
