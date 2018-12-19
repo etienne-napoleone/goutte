565d8a5821c45c1397765126c4294036a29341ae
etienne-napoleone <etienne@tomochain.com>
2018-12-19 16:26:28 +0700

    feat: add droplets fetching and parsing

--- goutte/main.py
+++ goutte/main.py
@@ -42,3 +42,21 @@ def _load_config(config: click.File) -> Dict[str, Dict]:
                      .format(config.name, e))
         sys.exit(1)
 
+
+def _get_droplets(names: List[str]) -> List[digitalocean.Droplet]:
+    """Get the droplets objects from the configuration doplets names"""
+    try:
+        manager = digitalocean.Manager(token=token)
+        droplets = manager.get_all_droplets()
+        return [droplet for droplet in droplets if droplet.name in names]
+    except digitalocean.baseapi.TokenError as e:
+        log.error(f'Token not valid: {e}')
+    except digitalocean.baseapi.DataReadError as e:
+        log.error(f'Could not read response: {e}')
+    except digitalocean.baseapi.JSONReadError as e:
+        log.error(f'Could not parse json: {e}')
+    except digitalocean.baseapi.NotFoundError as e:
+        log.error(f'Ressource not found: {e}')
+    except Exception as e:
+        log.error(f'Unexpected exception: {e}')
+
