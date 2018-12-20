9c27875b664390401dda391b37e76712ca3967da
etienne-napoleone <etienne@tomochain.com>
2018-12-20 21:44:47 +0700

    tests: add tests for `_prune_droplet_snapshots()`

--- tests/mock.py
+++ tests/mock.py
@@ -1,3 +1,19 @@
 class Snapshot:
-    def __init__(self, created_at):
+    def __init__(self, created_at=None, name=None):
         self.created_at = created_at
+        self.name = name
+
+    def destroy(self):
+        print(self.name)
+
+
+class Volume:
+    def __init__(self, name=None, snapshots=None, throw=None):
+        self.name = name
+        self.snapshots = snapshots
+        self.throw = throw
+
+    def get_snapshots(self):
+        if self.throw:
+            raise self.throw
+        return self.snapshots
--- tests/test_goutte.py
+++ tests/test_goutte.py
@@ -1,3 +1,5 @@
+import digitalocean
+
 from goutte import __version__
 from goutte import main
 from tests import mock
@@ -5,6 +7,43 @@ from tests import mock
 
 def test_version():
     assert __version__ == '1.0.0'
+
+
+def test_prune_droplet_snapshots(capsys):
+    volume = mock.Volume('testvol', [
+        mock.Snapshot(name='goutte-snapshot1', created_at='2018'),
+        mock.Snapshot(name='goutte-snapshot2', created_at='2017'),
+        mock.Snapshot(name='goutte-snapshot3', created_at='2016'),
+    ])
+    main._prune_volume_snapshots(volume, 1)
+    deleted = capsys.readouterr()
+    assert "goutte-snapshot1" not in deleted.out
+    assert "goutte-snapshot2" in deleted.out
+    assert "goutte-snapshot3" in deleted.out
+
+
+def test_prune_droplet_snapshots_goutte_prefix_only(capsys):
+    volume = mock.Volume('testvol', [
+        mock.Snapshot(name='snapshot1', created_at='2018'),
+        mock.Snapshot(name='snapshot2', created_at='2017'),
+    ])
+    main._prune_volume_snapshots(volume, 1)
+    deleted = capsys.readouterr()
+    assert "snapshot1" not in deleted.out
+    assert "snapshot2" not in deleted.out
+
+
+def test_prune_droplet_snapshots_goutte_raise(caplog):
+    exceptions = [
+        digitalocean.baseapi.TokenError,
+        digitalocean.baseapi.DataReadError,
+        digitalocean.baseapi.JSONReadError,
+        digitalocean.baseapi.NotFoundError
+    ]
+    for exception in exceptions:
+        volume = mock.Volume(name='testvol', throw=exception)
+        with caplog.at_level('ERROR'):
+            main._prune_volume_snapshots(volume, 1)
 
 
 def test_order_snapshots():
