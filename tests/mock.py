acc8b34642fdc8c646c8bbc7c4b84f7a7d9ffc3d
etienne-napoleone <etienne@tomochain.com>
2018-12-20 21:03:19 +0700

    tests: add tests for `_order_snapshots()`

---
+++ tests/mock.py
@@ -0,0 +1,3 @@
+class Snapshot:
+    def __init__(self, created_at):
+        self.created_at = created_at
--- tests/test_goutte.py
+++ tests/test_goutte.py
@@ -1,5 +1,19 @@
 from goutte import __version__
+from goutte import main
+from tests import mock
 
 
 def test_version():
     assert __version__ == '1.0.0'
+
+
+def test_order_snapshots():
+    snapshots = [
+        mock.Snapshot(created_at='2014-01-26T11:20:14Z'),
+        mock.Snapshot(created_at='2018-12-26T16:41:44Z'),
+        mock.Snapshot(created_at='2018-12-26T16:40:98Z'),
+    ]
+    ordered_snapshots = main._order_snapshots(snapshots)
+    assert ordered_snapshots[0].created_at == '2014-01-26T11:20:14Z'
+    assert ordered_snapshots[1].created_at == '2018-12-26T16:40:98Z'
+    assert ordered_snapshots[2].created_at == '2018-12-26T16:41:44Z'
